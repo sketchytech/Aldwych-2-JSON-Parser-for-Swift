6d034107c0512ef3eb22b13fd6c27d1bb5adf198
Anthony Levings <anthonylevings@As-iMac.local>
2015-08-03 21:07:16 +0100

    Equatable and contains

--- Aldwych2.playground/Pages/Equatable.xcplaygroundpage/Contents.swift
+++ Aldwych2.playground/Pages/Equatable.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@ import Foundation
 
 /*: 
 # Equatable
-Work has begun on adopting Equatable as you see here. Comparison of JSONDictionary to JSONValue and JSONArray to JSONValue to follow.
+A broad range of comparisons 
 */
 JSONValue(value:"String") == JSONValue(value:"Strung")
 JSONValue(value:"String") == JSONValue(value:"String")
@@ -24,9 +24,11 @@ JSONDictionary(dictionary:["String":1,"Strung":2]) == JSONValue(dictionary:["Swi
 if JSONValue(dictionary:["Swing":["String":1,"Strung":2]])["Swing"] != nil {
    JSONValue(dictionary:["Swing":["String":1,"Strung":2]])["Swing"]! == JSONDictionary(dictionary:["String":1,"Strung":2])
 }
-
+//: Since JSONArray and JSDictionary adopt SequenceType and their elements (JSONValue) adopt Equatable, it is also possible to use contains()
 JSONArray(array: ["String","Strung"]).contains("String")
 JSONArray(array: ["String","Strung",2,3,4]).contains(4)
-
-
+//: JSONArray and JSONDictionary also adopt Equatable themselves, and so [JSONDictionary], [JSONArray] and [JSONValue] arrays can all be compared
+[JSONArray(array: ["String","Strung"]),JSONArray(array: ["String","Strung"]), JSONArray(array: ["String","Strung",2,3,4])] == [JSONArray(array: ["String","Strung"]),JSONArray(array: ["String","Strung"]), JSONArray(array: ["String","Strung",2,3,4])]
+//: And contains used with them as well:
+[JSONArray(array: ["String","Strung"]),JSONArray(array: ["String","Strung"]), JSONArray(array: ["String","Strung",2,3,4])].contains(JSONArray(array: ["String","Strung"]))
 //: [Next](@next)
